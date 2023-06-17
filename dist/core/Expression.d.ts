65e1cf5999fb72bc5c0b84310d1f3e710dae0cf5
Jack Le Hamster <jacklehamster@gmail.com>
2023-06-16 18:16:08 -0700

    Update

--- dist/core/Expression.d.ts
+++
@@ -1,3 +0,0 @@
-interface Expression {
-    type: "expression";
-}
--- dist/core/convertor/convert-reference.d.ts
+++
@@ -1,5 +0,0 @@
-import { ReferenceAction } from "../actions/ReferenceAction";
-import { ExecutionStep } from "../execution/ExecutionStep";
-import { ConvertBehavior, Utils } from "./Convertor";
-import { ActionConvertorList } from "./convert-action";
-export declare function convertReferenceProperty<T>(action: ReferenceAction, results: ExecutionStep[], utils: Utils<T & ReferenceAction>, external: Record<string, any>, actionConversionMap: ActionConvertorList): Promise<ConvertBehavior | void>;
