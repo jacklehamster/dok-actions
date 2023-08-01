36dd48d9a41fd883281015089e2958dc5f5db20c
Jack Le Hamster <jacklehamster@gmail.com>
2023-07-31 23:37:56 -0700

    n

--- dist/core/resolutions/BooleanResolution.d.ts
+++ dist/core/resolutions/BooleanResolution.d.ts
@@ -1,2 +1,3 @@
+import { ObjectResolution } from "./ObjectResolution";
 import { Formula } from "./formula/Formula";
-export declare type BooleanResolution = boolean | Formula | undefined;
+export declare type BooleanResolution = boolean | Formula | ObjectResolution | undefined;
--- dist/core/resolutions/NumberResolution.d.ts
+++ dist/core/resolutions/NumberResolution.d.ts
@@ -1,2 +1,3 @@
+import { ObjectResolution } from "./ObjectResolution";
 import { Formula } from "./formula/Formula";
-export declare type NumberResolution<T extends number = number> = T | Formula | undefined;
+export declare type NumberResolution<T extends number = number> = T | Formula | ObjectResolution | undefined;
